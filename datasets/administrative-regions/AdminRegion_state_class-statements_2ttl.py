4710651c0ddaffe4044e9f49ee935f2a42a9cf27
David Kedrowski <96209091+dkedrowski@users.noreply.github.com>
2024-11-27 13:24:24 -0500

    Update AdminRegion_state_class-statements_2ttl.py

--- datasets/administrative-regions/AdminRegion_state_class-statements_2ttl.py
+++ datasets/administrative-regions/AdminRegion_state_class-statements_2ttl.py
@@ -16,18 +16,17 @@ Under ### Prefixes ###, define a list of
     rdf prefixes needed for the AdminRegion class statements (levels 1-3)
 """
 ### State ###
-state_abbrev = 'il'
+state_abbr = 'il'
 state_fips = '17'
 state_iri = 'kwgr:administrativeRegion.USA.' + state_fips
 
 ### Input FIles ###
 input1 = 'ttl_files/AdministrativeRegion_1/us_admin-regions_level-1.ttl'
-input2 = 'ttl_files/AdministrativeRegion_2/' + state_abbrev + '_' + state_fips + '_admin-regions_level-2.ttl'
-input3 = 'ttl_files/AdministrativeRegion_3/' + state_abbrev + '_' + state_fips + '_admin-regions_level-3.ttl'
-inputs = [input1, input2, input3]
+input2 = 'ttl_files/AdministrativeRegion_2/' + state_abbr + '_' + state_fips + '_admin-regions_level-2.ttl'
+input3 = 'ttl_files/AdministrativeRegion_3/' + state_abbr + '_' + state_fips + '_admin-regions_level-3.ttl'
 
 ### Output File ###
-output = 'ttl_files/class_statements/' + state_abbrev + '_' + state_fips + 'admin-region_class-statements.ttl'
+output = 'ttl_files/class_statements/' + state_abbr + '_' + state_fips + '_admin-region_class-statements.ttl'
 
 ### Prefixes ###
 prefixes = ['@prefix dcgeoid: <https://datacommons.org/browser/geoId/> .',
@@ -36,18 +35,17 @@ prefixes = ['@prefix dcgeoid: <https://datacommons.org/browser/geoId/> .',
             '@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .']
 
 # Create the output .ttl file
-with open (output, 'w') as outfile:
+with open(output, 'w') as outfile:
     # Add the prefixes to the output file
     for prefix in prefixes:
         outfile.write(prefix)
         outfile.write('\n')
     outfile.write('\n')
-    with open (input1, 'r') as infile:
+    with open(input1, 'r') as infile:
         for line in infile:
             # Finds the class statement for AdministrativeRegion_1
             if state_iri in line:
                 outfile.write(line.replace(';', '.'))  # Making sure syntax is correct
-    # Find the class statements in each input file and write them to the output file
     for input in [input2, input3]:
         with open(input, 'r') as infile:
             for line in infile:
